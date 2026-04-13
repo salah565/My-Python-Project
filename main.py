0a54a86989b0a9f400828c3a41bd016e1a1f1532
salah565 <salahmed92005@gmail.com>
2026-04-13 23:37:47 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,19 @@
+﻿low = 1
+high = 100
+
+print("Think of a number between 1 and 100.")
+print("Answer only with: yes or no")
+
+while low < high:
+    mid = (low + high) // 2
+
+    answer = input(f"Is your number greater than {mid}? ").strip().lower()
+
+    if answer == "yes":
+        low = mid + 1
+    elif answer == "no":
+        high = mid
+    else:
+        print("Please answer with yes or no.")
+
+print("Your number is:", low)
